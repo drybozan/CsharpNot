1d552470bd086bd5aedc3ef80df3da73584ec8b3
Derya Bozan <86890722+drybozan@users.noreply.github.com>
2021-08-06 14:44:16 +0300

    Add files via upload

---
+++ inheritanceUyg6/Calisan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inheritanceUyg6
+{
+   public class Calisan
+    {
+        public int yas = 67;
+        public Calisan() : this(47)
+        {
+            yas = 57;
+            MessageBox.Show("3");
+            yas = 89;
+        }
+        public Calisan(int yas)
+        {
+           yas = 107;
+           MessageBox.Show("4");
+        }
+    }
+}
---
+++ inheritanceUyg6/Mudur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inheritanceUyg6
+{
+   public class Mudur:Calisan
+    {
+        public Mudur():this(47)
+        {
+            yas = 807;
+            MessageBox.Show("1");
+        }
+        public Mudur(int yas) : base(97)
+        {
+            yas = 707;
+            MessageBox.Show("2");
+        }
+    }
+}
