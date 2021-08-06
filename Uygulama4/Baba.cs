37c3ea300b515e62473cb510b2d7d42206f00be9
Derya Bozan <86890722+drybozan@users.noreply.github.com>
2021-08-06 14:47:50 +0300

    Add files via upload

---
+++ Uygulama4/Baba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama4
+{
+    public class Baba : Dede
+    {
+        public Baba() : this("Sinan", 555)
+        {
+
+            MessageBox.Show("Parametresiz Baba ");
+        }
+        public Baba(string ad, int yas)// : base("Toklu", 222)//dede clasına gider parametreli metodu çalıştırır
+        {
+            MessageBox.Show("Parametreli Baba ");
+        }
+    }
+}
---
+++ Uygulama4/Dede.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iUygulama4
+{
+    public class Dede
+    {
+        public Dede() : this("Sinan", 555)
+        {
+
+            MessageBox.Show("Parametresiz Dede ");
+        }
+        public Dede(string ad,int yas) :base()//aynı şekil burası çalışır
+        {
+
+            MessageBox.Show(" Parametreli Dede ");
+        }
+       
+
+    }
+}
